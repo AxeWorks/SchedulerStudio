90840bb8a432018585ee97050d1489352356dff7
H8244 <h8244@student.jamk.fi>
2016-03-30 17:13:42 +0300

    Restore

---
+++ Scheduler studio/BusinessLogic.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Scheduler_studio
+{
+    
+    class Worker : INotifyPropertyChanged
+    {
+        #region PROPERTIES
+        private string firstname;
+
+        public string Firstname
+        {
+            get { return firstname;  }
+            set { firstname = value; Notify("Fname"); }
+            
+        }
+
+        private string lastname;
+
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; Notify("Lname"); }
+        }
+
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; Notify("Address"); }
+        }
+
+        private string phone;
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; Notify("Phone"); }
+        }
+
+        private DateTime regdate;
+
+        public DateTime RegDate
+        {
+            get { return regdate; }
+            set { regdate = value; Notify("RegDate"); }
+        }
+
+        private string otherInfo;
+
+        public string OtherInfo
+        {
+            get { return otherInfo; }
+            set { otherInfo = value; Notify("Other"); }
+        }
+        
+        #endregion
+        #region CONSTRUCTORS
+        public Worker() { }
+        public Worker(string fname, string lname, string addr, string phone, DateTime rdate, string other)
+        {
+            firstname = fname;
+            lastname = lname;
+            address = addr;
+            regdate = rdate;
+            otherInfo = other;
+        }
+        #endregion
+        #region METHODS
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void Notify(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        #endregion
+    }
+
+    static class BLData
+    {
+        #region WORKER
+        
+        public static void AddWorker(string fname, string lname, string address, string phone, DateTime regdate, string other)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+    }
+
+    /* public class MyData
+ {
+     public MyData(string name, bool data) { nameData = name; showData = data; }
+     public string nameData { get; set; }
+     public bool showData { get; set; }
+ }
+ public class MyDataGridTemplateColumn : DataGridTemplateColumn
+ {
+     public string ColumnName
+     {
+         get;
+         set;
+     }
+
+     protected override System.Windows.FrameworkElement GenerateElement(DataGridCell cell, object dataItem)
+     {
+         ContentPresenter cp = (ContentPresenter)base.GenerateElement(cell, dataItem);
+         BindingOperations.SetBinding(cp, ContentPresenter.ContentProperty, new Binding(this.ColumnName));
+         return cp;
+     }
+ }*/
+}
---
+++ Scheduler studio/MainWindow.xaml.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scheduler_studio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        DataView dv;
+        DataTable dt;
+        DataRow dr;
+
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        private void Row_Changed(object sender, DataRowChangeEventArgs e)
+        {
+            try
+            {
+                //e.Row.
+                MessageBox.Show(e.Row.RowState.ToString());
+               // MessageBox.Show("ROW CHANGED, dr[Lname].ToString():" + dr["Lname"].ToString() + ",\n    e.Row[Lname].ToString(): " + e.Row["Lname"].ToString());
+
+               /* string property = "";
+                string newData = "";
+
+                if (dr["Fname"].ToString() != e.Row["Fname"].ToString())
+                {
+                    property = "Fname";
+                    newData = e.Row["Fname"].ToString();
+                }
+
+                else if (dr["Lname"].ToString() != e.Row["Lname"].ToString())
+                {
+                    property = "Lname";
+                    newData = e.Row["Lname"].ToString();
+                }
+
+                else if (dr["Addr"].ToString() != e.Row["Addr"].ToString())
+                {
+                    property = "Addr";
+                    newData = e.Row["Addr"].ToString();
+                }
+
+                else if (dr["Phone"].ToString() != e.Row["Phone"].ToString())
+                {
+                    property = "Phone";
+                    newData = e.Row["Phone"].ToString();
+                }
+                else if (dr["RegDate"].ToString() != e.Row["RegDate"].ToString())
+                {
+                    property = "RegDate";
+                    DateTime RegDate = Convert.ToDateTime(e.Row["RegDate"].ToString());
+                    newData = RegDate.Year + "-" + RegDate.Month + "-" + RegDate.Day;
+                }
+                else if (dr["Other"].ToString() != e.Row["Other"].ToString())
+                {
+                    property = "Other";
+                    newData = e.Row["Other"].ToString();
+                }
+
+                MessageBox.Show("Property: " + property + ", newData: "+ newData);
+                BLData.UpdateWorker(dr, e.Row);*/
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnStaff_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                RefreshWorkers();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }            
+        }        
+
+        private void btnNotes_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void RefreshWorkers()
+        {
+            dt = DBWorker.GetAllWorkersData();
+            dv = dt.DefaultView;
+            dgWorkerList.ItemsSource = dv;
+        }
+
+        /* private void dgWorkerList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+         {
+             try
+             {
+                 sbWorkerDetails.DataContext = dgWorkerList.SelectedItem;
+             }
+             catch (Exception ex)
+             {
+                 MessageBox.Show(ex.Message);                
+             }
+         }
+
+
+         
+
+        private void btnDeleteWorker_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DateTime date = Convert.ToDateTime(dpDate.SelectedDate.Value);
+                DBWorker.RemoveWorker(txtFirstName.Text, txtLastName.Text, txtAddress.Text, txtPhone.Text, date, txtOther.Text);
+                dt = DBWorker.GetAllWorkersData();
+                dv = dt.DefaultView;
+                dgWorkerList.DataContext = dv;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }*/
+        /*
+                public string[] dates = { "RegDate", "ReservationDate" };
+
+
+                public void addColumnTemplates(object sender, DataGridAutoGeneratingColumnEventArgs e)
+                {
+
+                    string header = e.Column.Header.ToString();
+
+                    if (dates.Contains(header))
+                    {
+                        MyDataGridTemplateColumn col = new MyDataGridTemplateColumn();
+                        col.ColumnName = e.PropertyName;
+                        col.CellTemplate = (DataTemplate)FindResource("datePickerTemplate");
+                        e.Column = col;
+                        e.Column.Header = e.PropertyName;
+                    }
+
+                }*/
+
+        private void btnSaveChanges_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DBWorker.UpdateWorker(dt);
+                RefreshWorkers();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnShowSavePanel_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                spAddWorker.Visibility = Visibility.Visible;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSaveWorker_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Haluatko varmasti lisätä tämän käyttäjän?\n" + "Nimi: " + txtFname.Text + " " + txtLname.Text + "\n" + "Osoite: " + txtAddress.Text + "\n" + "Puhelinnumero: " + txtPhone.Text + "\n" + "Rekisteröintipäivä: " + dpRegDate.SelectedDate.Value.Day + "." + dpRegDate.SelectedDate.Value.Month + "." + dpRegDate.SelectedDate.Value.Year + "\n" + "Muu tieto: " + txtOther.Text, "Varmistus", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    dr = dt.NewRow();
+                    dr["fname"] = txtFname.Text;
+                    dr["lname"] = txtLname.Text;
+                    dr["addr"] = txtAddress.Text;
+                    dr["phone"] = txtPhone.Text;
+                    dr["regdate"] = dpRegDate.SelectedDate.Value.Day + "." + dpRegDate.SelectedDate.Value.Month + "." + dpRegDate.SelectedDate.Value.Year;
+                    dr["other"] = txtOther.Text;
+                    dt.Rows.Add(dr);
+                    spAddWorker.Visibility = Visibility.Collapsed;
+                    txtFname.Text = "";
+                    txtLname.Text = "";
+                    txtAddress.Text = "";
+                    txtPhone.Text = "";
+                    dpRegDate.Text = "";
+                    txtOther.Text = "";
+
+                    DBWorker.UpdateWorker(dt);
+                    RefreshWorkers();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
